353a968d9d41fcaf360fc16d8f35d1494ef7c7ce
Ber5h <102763351+Ber5h@users.noreply.github.com>
2022-05-05 21:00:31 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -22,15 +22,23 @@ namespace Английская_система_мер_длины
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+         private void button1_Click(object sender, EventArgs e)
         {
+            double inch;
             try
             {
-                label2.Text = $"{Inch((int)label1.Text):f1}";
+                if (double.Parse(textBox1.Text) > 0)
+                {
+                    inch = Inch(double.Parse(textBox1.Text));
+                    label2.Text = $"{inch:f2} дюймов\n{inch * 12:f2} футов\n{inch * 36:f2}" +
+                    $" ярдов\n{inch * 36 * 1760:f2} миль\n{inch * 36 * 1760 * 3.452:f3} морских лиг";//ААААААААААААААААААААААААААААААААААААААААААААААААААААААА
+                }
+                else label2.Text = "Введено неположительное значение";
+                
             }
-            catch(FormatException)
+            catch (FormatException)
             {
-
+                label2.Text = "введено нечисловое значение";
             }
         }
         private double Inch(double km)
